4f8f521be654246823898af9a4a420cfcca6edfb
Sun Guoqiang <1015263819@qq.com>
2024-02-14 20:42:22 +0800

    增删改查测试

--- admin/src/main/java/com/sgq/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/com/sgq/shortlink/admin/ShortLinkAdminApplication.java
@@ -1,5 +1,6 @@
 package com.sgq.shortlink.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author SunGuoqiang
  */
 @SpringBootApplication
+@MapperScan("com.sgq.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
