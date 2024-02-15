bbf70f814d188ffcdcf07b3c81e4c46af4568f25
Sun Guoqiang <1015263819@qq.com>
2024-02-15 15:32:56 +0800

    开发短链接后管系统用户注册相关

--- admin/src/main/java/com/sgq/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/sgq/shortlink/admin/controller/UserController.java
@@ -3,13 +3,12 @@ package com.sgq.shortlink.admin.controller;
 import cn.hutool.core.bean.BeanUtil;
 import com.sgq.shortlink.admin.common.convention.result.Result;
 import com.sgq.shortlink.admin.common.convention.result.Results;
+import com.sgq.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.sgq.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.sgq.shortlink.admin.dto.resp.UserRespDTO;
 import com.sgq.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 用户管理控制层
@@ -34,5 +33,22 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
+
+    /**
+     * 查询用户名是否存在
+     */
+    @GetMapping("/api/shortlink/v1/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username) {
+        return Results.success(userService.hasUsername(username));
+    }
+
+    /**
+     * 注册用户
+     */
+    @PostMapping("/api/shortlink/v1/user")
+    public Result<Void> register(@RequestBody UserRegisterReqDTO requestParam) {
+        userService.register(requestParam);
+        return Results.success();
     }
 }
--- admin/src/main/java/com/sgq/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/sgq/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,32 +1,57 @@
 package com.sgq.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sgq.shortlink.admin.common.convention.exception.ClientException;
-import com.sgq.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.sgq.shortlink.admin.dao.entity.UserDO;
 import com.sgq.shortlink.admin.dao.mapper.UserMapper;
+import com.sgq.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.sgq.shortlink.admin.dto.resp.UserRespDTO;
 import com.sgq.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.sgq.shortlink.admin.common.enums.UserErrorCodeEnum.*;
 
 /**
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class).eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
         if (userDO == null) {
-            throw new ClientException(UserErrorCodeEnum.USER_NULL);
+            throw new ClientException(USER_NULL);
         }
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public Boolean hasUsername(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUsername(requestParam.getUsername())) {
+            throw new ClientException(USER_NAME_EXIST);
+        }
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (inserted < 1) {
+            throw new ClientException(USER_SAVE_ERROR);
+        }
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
