a54f92b7ec593c502fb21421dac2eb611bd068c2
Sun Guoqiang <1015263819@qq.com>
2024-02-21 18:10:43 +0800

    feature：短链接分页新增创建时间查询和序列化

--- project/src/main/java/com/sgq/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/sgq/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -75,7 +75,8 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
         LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
                 .eq(ShortLinkDO::getGid, requestParam.getGid())
                 .eq(ShortLinkDO::getEnableStatus, 0)
-                .eq(BaseDO::getDelFlag, 0);
+                .eq(BaseDO::getDelFlag, 0)
+                .orderByDesc(ShortLinkDO::getCreateTime);
         IPage<ShortLinkDO> resultPage = baseMapper.selectPage(requestParam, queryWrapper);
         return resultPage.convert(each -> BeanUtil.toBean(each, ShortLinkPageRespDTO.class));
     }
@@ -94,6 +95,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 break;
             }
             customGenerateCount++;
+            
         }
         return shortUri;
     }
