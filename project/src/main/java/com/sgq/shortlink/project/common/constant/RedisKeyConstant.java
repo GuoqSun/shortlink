577f7cf7348ced540c353983ba6e3734b6a97b98
sunguoqiang <1015263819@qq.com>
2024-02-23 13:50:41 +0800

    feature：短链接跳转原始链接开发防止缓存穿透功能

--- project/src/main/java/com/sgq/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/com/sgq/shortlink/project/common/constant/RedisKeyConstant.java
@@ -11,6 +11,11 @@ public class RedisKeyConstant {
     public static final String GOTO_SHORT_LINK_KEY = "shortlink_goto_%S";
 
     /**
+     * 短链接空值跳转前缀
+     */
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "shortlink_is_null_goto_%S";
+
+    /**
      * 短链接跳转锁前缀
      */
     public static final String LOCK_GOTO_SHORT_LINK_KEY = "shortlink_lock_goto_%S";
