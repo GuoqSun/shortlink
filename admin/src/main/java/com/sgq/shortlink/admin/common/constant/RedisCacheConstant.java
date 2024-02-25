bfeae412535c2f071cbf8dc62f88f323a19a50ca
sunguoqiang <1015263819@qq.com>
2024-02-25 14:23:50 +0800

    optimize：优化单个用户创建分组数量不能大于20

--- admin/src/main/java/com/sgq/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/sgq/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -4,5 +4,14 @@ package com.sgq.shortlink.admin.common.constant;
  * 短链接后管 Redis 缓存常量类
  */
 public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "shortlink:lock_user-register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "shortlink:lock_group-create:%s";
 }
