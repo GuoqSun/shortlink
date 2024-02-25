a66f7ecbc38d89dc895acbb05422bb6216d2d310
sunguoqiang <1015263819@qq.com>
2024-02-25 15:28:14 +0800

    feature：短链接中台风控开发

---
+++ project/src/main/java/com/sgq/shortlink/project/handler/CustomBlockHandler.java
@@ -0,0 +1,16 @@
+package com.sgq.shortlink.project.handler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.sgq.shortlink.project.common.convention.result.Result;
+import com.sgq.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.sgq.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+
+/**
+ * 自定义流控策略
+ */
+public class CustomBlockHandler {
+
+    public static Result<ShortLinkCreateRespDTO> createShortLinkBlockHandlerMethod(ShortLinkCreateReqDTO requestParam, BlockException exception) {
+        return new Result<ShortLinkCreateRespDTO>().setCode("B100000").setMessage("当前访问网站人数过多，请稍后再试...");
+    }
+}
