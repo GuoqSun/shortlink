d63518040a58b91754209fb7ef724aa9e3961bc5
sunguoqiang <1015263819@qq.com>
2024-02-26 11:36:01 +0800

    fix：修复聚合服务部署404问题

--- admin/src/main/java/com/sgq/shortlink/admin/config/DataBaseConfiguration.java
+++ admin/src/main/java/com/sgq/shortlink/admin/config/DataBaseConfiguration.java
@@ -3,14 +3,14 @@ package com.sgq.shortlink.admin.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration("dataBaseConfigurationByAdmin")
 public class DataBaseConfiguration {
     @Bean
-    @ConditionalOnBean
+    @ConditionalOnMissingBean
     public MybatisPlusInterceptor mybatisPlusInterceptorByAdmin() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
--- project/src/main/java/com/sgq/shortlink/project/config/DataBaseConfiguration.java
+++ project/src/main/java/com/sgq/shortlink/project/config/DataBaseConfiguration.java
@@ -3,14 +3,14 @@ package com.sgq.shortlink.project.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataBaseConfiguration {
     @Bean
-    @ConditionalOnBean
+    @ConditionalOnMissingBean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
